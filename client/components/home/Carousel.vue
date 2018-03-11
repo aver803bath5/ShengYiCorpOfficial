a12b18744955e0c3f4371c3fed1849c63e866a75
g9Man <aver803bath261@gmail.com>
2018-03-11 10:18:11 +0800

    feat/carousels: resize event listener and function

--- client/components/home/Carousel.vue
+++ client/components/home/Carousel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="carousel-wrapper" v-loading="loading">
-    <el-carousel :interval="4000" type="card" height="500px">
+    <el-carousel :interval="4000" :type="carouselType" :height="carouselHeight">
       <el-carousel-item v-for="item in carousels.banners" :key="item.id" :style="{ backgroundImage: `url(${item.url})` }">
       </el-carousel-item>
     </el-carousel>
@@ -11,18 +11,45 @@
 import { mapState } from 'vuex';
 
 export default {
+  mounted() {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize);
+  },
   data() {
     return {
       loading: false,
-      height: 0,
-      mode: '',
-      project: '123'
+      carouselHeight: '0',
+      carouselType: '',
     };
   },
   computed: {
     ...mapState([
       'carousels'
     ])
+  },
+  methods: {
+    handleResize() {
+      const screenWidth = window.innerWidth;
+      if (screenWidth <= 480) {
+        this.carouselHeight = "300px";
+        this.carouselType = '';
+      } else if (screenWidth <= 800) {
+        this.carouselHeight = "500px";
+        this.carouselType = '';
+      } else if (screenWidth <= 1680) {
+        this.carouselHeight = "400px";
+        this.carouselType = 'card';
+      } else if (screenWidth <= 1920) {
+        this.carouselHeight = "800px";
+        this.carouselType = 'card';
+      } else {
+        this.carouselHeight = '800px';
+        this.carouselType = 'card';
+      }
+    }
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize);
   },
 }
 </script>
