1c30e855d5166401be0776bd93d50d84f9626e35
g8Man <aver803bath261@gmail.com>
2018-03-14 14:05:34 +0800

    feat/products: products pages rough layout

--- client/pages/_lang/products-list/_catID/_page/_search.vue
+++ client/pages/_lang/products-list/_catID/_page/_search.vue
@@ -5,11 +5,26 @@
       產品介紹
     </h1>
   </header>
+  <el-row>
+    <el-col :span="8">
+      <el-card class="box-card">
+        <div v-for="category in productsListCategories" :key="category.id" class="text item">
+          {{ category.name }}
+        </div>
+      </el-card>
+    </el-col>
+    <el-col :span="16">
+      <div v-for="product in productsListProducts" :key="product.id">
+        {{ product }}
+      </div>
+    </el-col>
+  </el-row>
 </div>
 </template>
 
 <script>
 import axios from '~/plugins/axios';
+import { mapState } from 'vuex';
 
 export default {
   validate({ params }) {
@@ -29,11 +44,25 @@ export default {
     store.commit('SET_PRODUCTSLISTCATEGORIES', catRes.data.categories);
     store.commit('SET_PRODUCTSLISTPRODUCTS', productsRes.data.products);
     store.commit('SET_LOADING', false);
-
   },
+  computed: {
+    ...mapState([
+      'productsListCategories',
+      'productsListProducts'
+    ])
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.wrapper {
+  padding: 1rem;
+}
+
+@media screen and (max-width: 950px) {
+  .wrapper {
+     margin-top: 4.5rem;
+  }
+}
 
 </style>
