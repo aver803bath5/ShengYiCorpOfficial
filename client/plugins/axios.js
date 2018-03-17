1c367c04efde28f4a11178cf36372c909c31ef33
aver803bath5 <aver803bath261@gmail.com>
2018-03-17 19:36:11 +0800

    feat/axios: use NODE_ENV to varify baseURL

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+let path = '';
+if (process.env.NODE_ENV === 'development') {
+  path = 'http://www.cyku.tw:9487/';
+} else {
+  path = 'http://www.shengyi.com.tw/api/';
+}
+
 export default axios.create({
-  baseURL: 'http://www.cyku.tw:9487/'
+  baseURL: path
 });
