c2ef532e04fdc8fb56225d83e45fa772799c8e9f
aver803bath5 <aver803bath261@gmail.com>
2018-03-17 13:28:24 +0800

    feat/brands: fetch data and display

---
+++ client/pages/_lang/brands.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <h1>
+        {{ $t('navbar.brands') }}
+      </h1>
+    </header>
+    <section>
+      <el-row type="flex" class="brands-row" justify="space-between" :gutter="20">
+        <el-col :sm="{span:8}" :xs="{span:24}" v-for="brand in brands" :key="brand.id" >
+          <nuxt-link :to="brand.link" class="brand-link">
+            <img :src="brand.picture" :alt="brand.link">
+          </nuxt-link>
+        </el-col>
+      </el-row>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from '~/plugins/axios';
+
+export default {
+  async asyncData({ store }) {
+    const { data } = await axios.get(`${store.state.locale}/brands`);
+    return { brands: data.brands };
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  max-width: 1300px;
+  margin: auto;
+  padding: 1rem;
+}
+
+.brand-link {
+  display: block;
+  margin-top: 1rem;
+  border: 1px #ddd solid
+}
+
+img {
+  display: block;
+  width: 100%;
+}
+
+.brands-row {
+  flex-wrap: wrap;
+}
+
+@media screen and (max-width: 1000px) {
+  .wrapper {
+    margin-top: 3rem;
+  }
+}
+</style>
---
+++ client/pages/brands.vue
@@ -0,0 +1,4 @@
+<script>
+  import Brands from '~/pages/_lang/brands';
+  export default Brands;
+</script>
