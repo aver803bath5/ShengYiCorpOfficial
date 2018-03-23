0e1bd7a440cb0d1fc8ac7bb20759602d5792e247
aver803bath5 <aver803bath261@gmail.com>
2018-03-23 23:39:09 +0800

    style/product-display: remove space arround for cards wrap

--- client/components/gas-products/ProductsDisplay.vue
+++ client/components/gas-products/ProductsDisplay.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="product-display-wrapper">
-    <el-row type="flex" :gutter="20" class="product-display-row" justify="space-around" v-if="gasProducts.length != 0">
+    <el-row :gutter="20" class="product-display-row" v-if="gasProducts.length != 0">
       <el-col v-for="product in gasProducts" :key="product.id"  :xs="{span:24}" :sm="{span:8}" class="product-card-wrapper">
         <nuxt-link :to="convertPath(`/gas/product/${product.id}`, locale)">
           <el-card :body-style="{ padding: '0px' }">
